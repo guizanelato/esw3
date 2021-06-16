0a38aaaba255e121cf8003dc215b49d0655bf0f8
Guilherme Zanelato <guilherme.zanelato@gmail.com>
2021-06-15 21:45:08 -0300

    fix: remoção de códigos de teste e atualização do requirements.txt

--- app/app.py
+++ app/app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template
 from routes.auth import blueprint as auth
 from routes.associado import blueprint as associado
 
-from models.Associado import Associado, AssociadoStore
-from models.Plano import Plano, PlanoStore
-from models.User import User,UserStore
-
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
@@ -14,6 +10,3 @@ app.register_blueprint(auth)
 app.register_blueprint(associado)
 
 
-@app.route("/teste")
-def teste():
-    return render_template("listar_clinica.html")
